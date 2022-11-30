2a397f290845e9b1a56bc727f659f24aabe82a67
李深 <lishen@9snail.com>
2022-11-30 17:59:08 +0800

    1.第二次测试

---
+++ project/test.py
@@ -0,0 +1,5 @@
+import datetime
+import sys
+sys.path.append("..")
+from config import name
+print('jenkins and docker test ok v1',name,'\n',datetime.datetime.now())
--- test.py
+++
@@ -1,2 +0,0 @@
-import datetime
-print('jenkins and docker test ok v1',datetime.datetime.now())
